276b7cbf72b3ea5bcb65f9c9424847ff53ac5183
Ringo233 <ringo233333@gamil.com>
2023-03-27 23:51:26 +0800

    Ringo

---
+++ phpsso_server/caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'port' => 3306,
+		'database' => 'code_slytherin_t',
+		'username' => 'code_slytherin_t',
+		'password' => 'rFwm5xsrz3HXESJS',
+		'tablepre' => 'v9_sso_',
+		'charset' => 'utf8',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		)
+);
+
+?>
